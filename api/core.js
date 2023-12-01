795d3bafff89ea5e4ed4c87665b77cd1482afba3
mdenitti <massimodn77@gmail.com>
2023-12-01 13:52:34 +0100

    update core.js

--- api/core.js
+++ api/core.js
@@ -1,49 +1,55 @@
 // get all core functions and methods
-const apiUrl = " http://localhost:3000/posts";
+const apiUrl = "http://localhost:3000/posts";
 const blog = document.getElementById("blog");
 
-
-// Get all posts
-const getLocalApi = async () => {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data;
+// Get all posts - services...
+const getLocalApi = () => {
+    return fetch(apiUrl)
+        .then(response => response.json())
 }
 
 // Display all posts
-const displayData = async () => {
-    const data = await getLocalApi();
-    console.log(data);
-    data.forEach(data => {
-        blog.innerHTML += `
-        <div class="card mb-2" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${data.title}</h5>
-                <p class="card-text">${data.body}</p>
-                <p class="card-text"><small class="text-muted">${data.author}</small></p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        `
-    });
+const displayData = () => {
+    getLocalApi()
+        .then(data => {
+            const sortedData = data.sort((a, b) => b.id - a.id); // Sort by id
+            blog.innerHTML = ""; // Clear existing content
+
+            sortedData.forEach(post => {
+                const card = document.createElement("div");
+                card.className = "card mb-2";
+                card.style = "width: 18rem;";
+
+                card.innerHTML = `
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title} ${post.id}</h5>
+                        <p class="card-text">${post.body}</p>
+                        <p class="card-text"><small class="text-muted">${post.author}</small></p>
+                        <button class="btn btn-primary" data-id="${post.id}" onclick="editPost(${post.id})">Edit</button>
+                        <button class="btn btn-danger" data-id="${post.id}" onclick="deletePost(${post.id})">Delete</button>
+                    </div>
+                `;
+
+                blog.appendChild(card);
+            });
+        });
 }
 
-
-// Post data to server
-const postLocalApi = async (postData) => {
-    const response = await fetch(apiUrl, {
+// Post data to server extraced from method becuz of service concept
+const postLocalApi = (postData) => {
+    return fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(postData)
-    });
-    const data = await response.json();
-    return data;
+    })
+        .then(response => response.json())
+        .then(data => data);
 }
 
 // Get data from form
-const submitData = async () => {
+const submitData = () => {
     const title = document.getElementById("title").value;
     const body = quill.getText();
     const author = document.getElementById("author").value;
@@ -52,23 +58,52 @@ const submitData = async () => {
         alert("Please fill all the fields");
         return;
     }
-    
+
     const postData = {
         title,
         body,
         author
     }
 
-    const response = await postLocalApi(postData);
+    postLocalApi(postData)
+        .then(() => {
+            // clear the form
+            document.getElementById("title").value = "";
+            document.getElementById("editor").value = "";
+            document.getElementById("author").value = "";
 
-    // clear the form
-    document.getElementById("title").value = "";
-    document.getElementById("editor").value = "";
-    document.getElementById("author").value = "";
-
-    displayData();
+            displayData();
+        });
 }
 
+// Edit post
+const editPost = (postId) => {
+    const newTitle = prompt("Enter the new title:");
+    if (newTitle !== null) {
+        fetch(`${apiUrl}/${postId}`, {
+            // fetch(apiUrl+"/"+postId), { concat alterntive
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: newTitle })
+        })
+            .then(response => response.json())
+            .then(() => displayData());
+    }
+}
+
+// Delete post
+const deletePost = (postId) => {
+    const confirmDelete = confirm("Are you sure you want to delete this post?");
+    if (confirmDelete) {
+        fetch(`${apiUrl}/${postId}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(() => displayData());
+    }
+}
 
 displayData();
 
